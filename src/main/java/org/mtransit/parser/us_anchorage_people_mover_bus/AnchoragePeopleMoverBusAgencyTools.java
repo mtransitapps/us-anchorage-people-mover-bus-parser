3422d5ffa5ae874f5f7090534e6e86b793b238f6
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-08 10:34:55 -0500

    GTFS Direction Finder > enable

--- src/main/java/org/mtransit/parser/us_anchorage_people_mover_bus/AnchoragePeopleMoverBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/us_anchorage_people_mover_bus/AnchoragePeopleMoverBusAgencyTools.java
@@ -8,9 +8,6 @@ import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.ColorUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -18,20 +15,15 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
+
+import static org.mtransit.commons.StringUtils.EMPTY;
 
 // https://www.muni.org/Departments/transit/PeopleMover/Pages/GTFSDiscliamer.aspx
 // http://gtfs.muni.org/People_Mover.gtfs.zip
@@ -157,91 +149,6 @@ public class AnchoragePeopleMoverBusAgencyTools extends DefaultAgencyTools {
 		return super.getRouteColor(gRoute);
 	}
 
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-
-	static {
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		//noinspection deprecation
-		map2.put(11L, new RouteTripSpec(11L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Gov't Hl", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Fairview") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(//
-								"2195", // 19TH AVENUE & CHUGACH MANOR #Fairview
-								"0525", // ++
-								"3527", // !=
-								"1450", // <> CITY HALL
-								"0002", // <>
-								"1253", // !=
-								"0856", // ++
-								"0857" // RICHARDSON VISTA & BLDG 12 WEST #GovtHl
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(//
-								"0857", // RICHARDSON VISTA & BLDG 12 WEST #GovtHl
-								"0858", // ++
-								"0861", // !=
-								"1450", // <> CITY HALL
-								"0002", // <>
-								"0734", // !=
-								"0524", // ++
-								"2195" // 19TH AVENUE & CHUGACH MANOR #Fairview
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(20L, new RouteTripSpec(20L, //
-				0, MTrip.HEADSIGN_TYPE_STRING, "AK Native Medical Ctr", // ANMC
-				1, MTrip.HEADSIGN_TYPE_STRING, "Downtown") //
-				.addTripSort(0, //
-						Arrays.asList(//
-								"2051", // DOWNTOWN TRANSIT CENTER
-								"1289", // ++
-								"3010" // DIPLOMACY & ANMC N
-						)) //
-				.addTripSort(1, //
-						Arrays.asList(//
-								"3010", // DIPLOMACY & ANMC N
-								"1342", // ++
-								"2051" // DOWNTOWN TRANSIT CENTER
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(21L, new RouteTripSpec(21L, //
-				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Mtn Vw", //
-				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Northway Mall") //
-				.addTripSort(MDirectionType.EAST.intValue(), //
-						Arrays.asList(//
-								"0514", // PENLAND PKWY & PAUL LALONE N
-								"7032", // ++
-								"1335" // PARSONS & LANE WNW
-						)) //
-				.addTripSort(MDirectionType.WEST.intValue(), //
-						Arrays.asList(//
-								"1335", // PARSONS & LANE WNW
-								"1339", // ++
-								"0514" // PENLAND PKWY & PAUL LALONE N
-						)) //
-				.compileBothTripSort());
-		//noinspection deprecation
-		map2.put(91L, new RouteTripSpec(91L, //
-				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Dimond Ctr", //
-				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Huffman Business Pk") //
-				.addTripSort(MDirectionType.NORTH.intValue(), //
-						Arrays.asList(//
-								"1017", // HUFFMAN BUSINESS PARK
-								"1557", // ++
-								"0057" // DIMOND TRANSIT CENTER
-						)) //
-				.addTripSort(MDirectionType.SOUTH.intValue(), //
-						Arrays.asList(//
-								"0057", // DIMOND TRANSIT CENTER
-								"1574", // ++
-								"1017" // HUFFMAN BUSINESS PARK
-						)) //
-				.compileBothTripSort());
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
 	private static final Pattern STARTS_WITH_PM_ = Pattern.compile("(^PM)", Pattern.CASE_INSENSITIVE);
 
 	@NotNull
@@ -252,40 +159,32 @@ public class AnchoragePeopleMoverBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public int compareEarly(long routeId, @NotNull List<MTripStop> list1, @NotNull List<MTripStop> list2, @NotNull MTripStop ts1, @NotNull MTripStop ts2, @NotNull GStop ts1GStop, @NotNull GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@NotNull
-	@Override
-	public ArrayList<MTrip> splitTrip(@NotNull MRoute mRoute, @Nullable GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@NotNull
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(@NotNull MRoute mRoute, @NotNull GTrip gTrip, @NotNull GTripStop gTripStop, @NotNull ArrayList<MTrip> splitTrips, @NotNull GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
-	@Override
 	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
 		mTrip.setHeadsignString(
 				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
 				gTrip.getDirectionIdOrDefault()
 		);
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	private static final Pattern INBOUND_OUTBOUND_ = CleanUtils.cleanWords("inbound", "outbound");
+
+	private static final Pattern ENDS_WITH_BOUNDS_ = Pattern.compile("( (\\w{3}|E|W|N|S)$)", Pattern.CASE_INSENSITIVE);
+
+	@NotNull
+	@Override
+	public String cleanDirectionHeadsign(boolean fromStopName, @NotNull String directionHeadSign) {
+		directionHeadSign = super.cleanDirectionHeadsign(fromStopName, directionHeadSign);
+		if (fromStopName) {
+			directionHeadSign = ENDS_WITH_BOUNDS_.matcher(directionHeadSign).replaceAll(EMPTY);
+		} else {
+			directionHeadSign = INBOUND_OUTBOUND_.matcher(directionHeadSign).replaceAll(EMPTY); // force last/first stop
+		}
+		return directionHeadSign;
 	}
 
 	@Override
@@ -308,6 +207,7 @@ public class AnchoragePeopleMoverBusAgencyTools extends DefaultAgencyTools {
 
 	private String[] getIgnoredWords() {
 		return new String[]{
+				"ANMC", "VA",
 				"ENE", "ESE",
 				"WNW", "WSW",
 				"NNE", "NNW",
